9b252e0eecabbbdc70f26e8eef14316bd1acd1ad
nwoodard1 <nwoodard1@cnm.edu>
2020-02-10 10:31:07 -0700

    Completed Variables and types prework screencast

---
+++ variables-and-data.js
@@ -0,0 +1,34 @@
+/*
+* Variables, Data Types, and Typing
+
+let wordData = "Word List";
+
+console.log(wordData);
+
+let subtotal = 100;
+let tax = subtotal * 0.07;
+let totalWithTax = tax + subtotal;
+ */
+/*let obj = {
+    key1: "value 1",
+    key2: 3,
+    obj2: {
+        obj2Key1: "Value of inner object",
+    }
+};
+
+console.log(obj.key1);*/
+/*let variable = "string";*/
+/*let var1 = "2";
+let var2 = 5;
+let result = var1 + var2;
+
+console.log(result);*/
+
+/*let add = 1 + 2;
+let sub = 2 -1;
+let mult = 2*4;
+let division = 4 / 2;
+let mod =5 % 2;
+let string = "String 1 " + "String 2";
+console.log(string);*/
